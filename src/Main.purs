60f36bac9a2fda8e1fc68b90591d18ccf75577f3
Chris LaRose <cjlarose@gmail.com>
2016-11-23 20:54:56 -0800

    Move origin to bottom-left

--- src/Main.purs
+++ src/Main.purs
@@ -5,7 +5,7 @@ import Control.Monad.Eff (Eff, forE)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Int (toNumber, floor)
 import Data.Maybe (Maybe(Just, Nothing))
-import Graphics.Canvas (getCanvasElementById, CANVAS, getContext2D, setFillStyle, fillRect, Context2D)
+import Graphics.Canvas (getCanvasElementById, CANVAS, getContext2D, setFillStyle, fillRect, Context2D, scale, transform, Transform, translate)
 
 fillPoint :: forall e. Context2D -> Int -> Int -> Eff ( canvas :: CANVAS | e ) Context2D
 fillPoint ctx x y = fillRect ctx { x: toNumber x, y: toNumber y, w: 1.0, h: 1.0 }
@@ -23,16 +23,22 @@ drawLine ctx color x0 y0 x1 y1 = do
 clear :: forall e. Context2D -> Eff ( canvas :: CANVAS | e ) Context2D
 clear ctx = do
   setFillStyle "rgb(0, 0, 0)" ctx
-  fillRect ctx { x: 0.0, y: 0.0, w: 800.0, h: 800.0 }
+  fillRect ctx { x: 0.0, y: 0.0, w: 800.0, h: 600.0 }
+
+transformIdentity :: Transform
+transformIdentity = { m11: 1.0, m21: 0.0, m31: 0.0,
+                      m12: 0.0, m22: 1.0, m32: 0.0 }
 
 main :: forall e. Eff (console :: CONSOLE, canvas :: CANVAS | e) Unit
 main = do
-  log "Hello sailor!"
   canvasElement <- getCanvasElementById "canvas"
   case canvasElement of
     Just element -> do
                      ctx <- getContext2D element
+                     translate { translateX: 0.0, translateY: 600.0 } ctx
+                     scale { scaleX: 1.0, scaleY: -1.0 } ctx
                      clear ctx
                      drawLine ctx "#ff0000" 0 0 100 100
-                     log "hello"
+                     transform transformIdentity ctx
+                     pure unit
     Nothing -> log "sorry"
