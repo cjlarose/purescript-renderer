ab4294045ebee43a7bf0128cd2e681e13fef9346
Chris LaRose <cjlarose@gmail.com>
2016-11-26 13:36:09 -0800

    Add face parsing to wireframe parser

--- src/Wireframe.purs
+++ src/Wireframe.purs
@@ -2,7 +2,7 @@ module Wireframe where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Data.Maybe (Maybe)
+import Data.Maybe (Maybe, fromJust)
 import DOM (DOM)
 import DOM.Node.Types (ElementId(..), documentToNonElementParentNode, elementToNode)
 import DOM.Node.Node (textContent)
@@ -14,9 +14,10 @@ import Data.Nullable (toMaybe)
 import Data.String (Pattern(..), split)
 import Data.String.Regex (test, regex)
 import Data.String.Regex.Flags (noFlags)
-import Data.Array (filter, unsafeIndex)
+import Data.Array (filter, unsafeIndex, head)
 import Data.Either (fromRight)
-import Global (readFloat)
+import Data.Int (floor)
+import Global (readFloat, readInt)
 import Partial.Unsafe (unsafePartial)
 
 getWireframeDataById :: forall e0 e1. String -> Eff ( dom :: DOM | e0 ) (Maybe (Eff ( dom :: DOM | e1) String))
@@ -28,8 +29,9 @@ getWireframeDataById elId = do
   let maybeEl = toMaybe nullableEl
   pure $ textContent <<< elementToNode <$> maybeEl
 
+newtype Face = Face { v0 :: Int, v1 :: Int, v2 :: Int }
 newtype Vec3 = Vec3 { x :: Number, y :: Number, z :: Number }
-newtype Model = Model { vertices :: Array Vec3 }
+newtype Model = Model { vertices :: Array Vec3, faces :: Array Face }
 
 isBlank :: String -> Boolean
 isBlank = test r
@@ -37,7 +39,7 @@ isBlank = test r
     r = unsafePartial fromRight $ regex "^\\s*$" noFlags
 
 parseWireframeData :: String -> Model
-parseWireframeData str = Model { vertices: vertices }
+parseWireframeData str = Model { vertices: vertices, faces: faces }
   where
     splitBySpace :: String -> Array String
     splitBySpace = split (Pattern " ")
@@ -52,3 +54,14 @@ parseWireframeData str = Model { vertices: vertices }
     vertices = vertexData <#> (\arr -> Vec3 { x: readFloat $ unsafePartial unsafeIndex arr 1,
                                               y: readFloat $ unsafePartial unsafeIndex arr 2,
                                               z: readFloat $ unsafePartial unsafeIndex arr 3 })
+
+    faceData :: Array (Array String)
+    faceData = filter (\l -> (unsafePartial unsafeIndex l 0) == "f") lines
+
+    vertexIndex :: String -> Int
+    vertexIndex = floor <<< readInt 10 <<< unsafePartial fromJust <<< head <<< split (Pattern "/")
+
+    faces :: Array Face
+    faces = faceData <#> (\arr -> Face { v0: vertexIndex $ unsafePartial unsafeIndex arr 1,
+                                         v1: vertexIndex $ unsafePartial unsafeIndex arr 2,
+                                         v2: vertexIndex $ unsafePartial unsafeIndex arr 3 })
